fe5801e4df20818ce49989657d51ab6b31545b55
Adrian Z <sylzyg555@interia.pl>
2025-11-23 17:29:46 +0100

    Angular + Sass setup

---
+++ SyntaxCore_Angular/src/app/main-console/main-console.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'main-console',
+  standalone: true,
+  templateUrl: './main-console.component.html',
+  styleUrl: './main-console.component.css'
+})
+export class MainConsoleComponent {
+
+}
